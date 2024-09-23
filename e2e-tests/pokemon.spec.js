cb508a174a2819d5a43484387eef0d81dcbae0d8
Emmanuel Selase Tormeti <emmasela04@gmail.com>
2024-09-23 16:38:35 +0000

    added playwright for e2e testing

---
+++ e2e-tests/pokemon.spec.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+const { test, describe, expect, beforeEach } = require('@playwright/test')
+
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('http://localhost:8081/')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+})
